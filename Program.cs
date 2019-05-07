adefe33973f40e3d4dd15ef41efb91a9d3122505
Nathan Woodruff <46133948+nwoodr94@users.noreply.github.com>
2019-05-07 11:24:17 -0500

    Add files via upload
    Hello World

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BasicsOfCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
+
