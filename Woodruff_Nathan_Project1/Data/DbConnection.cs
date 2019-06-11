eabd62ce5d9455ce77f0dfd53400578a12ee5ef9
Nathan Woodruff <nwoodr94@gmail.com>
2019-06-10 22:31:07 -0500

    Update

---
+++ Woodruff_Nathan_Project1/Data/DbConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public static class DbConnection
+    {
+        private static string Str = "Server=nwoodr94.database.windows.net;Database=PizzaBoxDb;user id=nathan;Password=liberty94!;";
+        public static String Connection
+        {
+            get
+            {
+                return DbConnection.Str;
+            }
+        }
+
+
+    }
+}
